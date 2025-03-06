13caf21294b32a7f615a6d9c1ec961189b746245
rottenkiwi <yonatanhornstein@gmail.com>
2025-03-06 19:45:46 +0200

    Added README and gitignore files

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 // Configuration file storing sensitive details like email, password, and language setting
-export const email = 'velixo_tester_06@hotmail.com'; // Email for login
-export const password = '04510451qQ'; // Password for login
+export const email = 'YOUR_EMAIL'; // Email for login
+export const password = 'YOUR_PASSWORD'; // Password for login
 export const language: string = 'HE'; // Language setting: 'EN' or 'HE'
